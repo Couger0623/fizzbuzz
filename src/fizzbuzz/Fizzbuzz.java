7cf3a466998242acffe9a0c6169c9a22386b07b8
Couger0623 <>
2022-09-30 21:03:19 +0900

    練習

---
+++ src/fizzbuzz/Fizzbuzz.java
@@ -0,0 +1,34 @@
+package fizzbuzz;
+
+public class Fizzbuzz {
+
+	public void fizzbuzz() {
+		for (int i = 1; i < 101; i++) {
+			String sys = fb(i);
+			System.out.println(sys);
+		}
+	}
+	
+	public String fb(int i) {
+		String a = fizz(i);
+		String s = i + ":";
+		return s + a;
+	}
+	
+	
+	
+
+	public String fizz(int i) {
+		String fb = "";
+		if (i % 3 == 0) {
+			fb += "fizz";
+		}
+
+		if (i % 5 == 0) {
+			fb += "bazz";
+		}
+			return fb;
+	
+	}
+
+}
--- src/fizzbuzz/Main.java
+++ src/fizzbuzz/Main.java
@@ -3,22 +3,10 @@ package fizzbuzz;
 public class Main {
 
 	public static void main(String[] args) {
+	
+		Fizzbuzz f = new Fizzbuzz();
+		f.fizzbuzz();
 		
-		
-		
-		for(int i = 1; i < 101; i++){
-			String fb = i + ":";
-			if(i % 3 == 0 ) {
-				fb += "fizz";
-			}
-			
-			if(i % 5 == 0) {
-				fb += "bazz";
-			}
-			System.out.println(fb);
-		}
-		
-
 	}
 
 }
